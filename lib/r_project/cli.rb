018f3483b96fc54673f12a6ae2c7f5f764e13b18
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-27 02:58:06 +0000

    first commit

---
+++ lib/r_project/cli.rb
@@ -0,0 +1,8 @@
+require "r_project/cli/version"
+
+module RProject
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/r_project/cli/version.rb
@@ -0,0 +1,5 @@
+module RProject
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
