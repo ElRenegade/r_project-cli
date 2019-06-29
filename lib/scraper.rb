8524d906fd0c75b61c3e558d3d85fece92147d2f
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-29 19:16:22 +0000

    completed method for collecting team info

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,13 +2,29 @@ require 'nokogiri'
 require 'open-uri'
 
 
-#class TeamScraper
- # def self.teams
-    html = open("https://www.mlb.com/team")
-    doc = Nokogiri::HTML(html)
-    teams = doc.css(".p-content-heading__text")
-    puts teams
-  #end
-#end
+# class TeamScraper
+#   def self.team_names
+    team_pro = []
+    
+    doc = Nokogiri::HTML(open("https://www.mlb.com/team"))
+    teams = doc.css("div.l-grid__content.l-grid__content--inset")
+    
+    teams.css("div.p-featured-content__body").each do |team|
+      name = team.css("div.u-text-h4.u-text-flow").text
+      location = team.css("div.p-featured-content__text").text.gsub(/\s+/, ' ')
+      team_url = team.css("a").attribute("href").value
+      team_info = {:name => name,
+            :location => location,
+            :team_url => team_url}
+            
+            team_pro << team_info
+        end
+      puts team_pro
+     #end
+  
+#   def 
+    
+#   end
+# end
 
 
