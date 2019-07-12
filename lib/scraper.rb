cc226b7deb1a4021fca4d88575cb77fba9c49e2d
Santiago Sanchez <chagostwo@yahoo.com>
2019-07-12 21:44:56 +0000

    changed sites to espn, mlb was unscrapable

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,66 +3,29 @@ require 'open-uri'
 
 
 # class TeamScraper
-  @doc = Nokogiri::HTML(open("https://www.mlb.com/team"))
+  @doc = Nokogiri::HTML(open("https://www.espn.com/mlb/team/_/name/laa/los-angeles-angels"))
 
 #   def self.teams
-    team_pro = []
+    team_pro = {}
 
-    teams = @doc.css("div.l-grid__content.l-grid__content--inset")
+    teams = @doc.css(".upcoming")
+    team_pro[:o_team] = teams.css(".game-info").text
+    team_pro[:date] = teams.css(".game-date").text
+    team_pro[:time] = teams.css(".time").text
+    puts team_pro
     
-    teams.css("div.p-featured-content__body").each do |team|
-      name = team.css("div.u-text-h4.u-text-flow").text
-      location = team.css("div.p-featured-content__text").text.strip      #gsub(/\s+/, ' ') 
-      team_url = team.css("p").css("a").attribute("href").value
-      team_info = {:name => name,
-            :location => location,
-            :team_url => team_url}
-            
-            team_pro << team_info
-        end
-      team_pro
-     #end
-  
-#   def self.team_name
-      # names = team_pro.map{|name| name[:name]}
-      
-      # names
-#   end
-
-  # def self.next_game
-    # team_site = team_pro.map{|url| url[:team_url]}
-    
-    # team_site
+   
+ 
+  # if team_pro[0].include?("CHICAGO WHITE SOX")
+  #   puts "yes"
+  # else 
+  #   puts "no"
   # end
   
-    # def self.get_schedule
-        #team_site.each{|site|
-          
-       # }
-        
-    # end
-    
-    
-    date_time = []
-    
-    schedule = Nokogiri::HTML(open("https://www.mlb.com/orioles")) 
-    
-    datentime = schedule.css("div.g5-component--mlb-scores__game g5-component--mlb-scores__game--MIG g5-component--mlb-scores__game--Preview")
-    datentime.css("div.g5-component--mlb-scores__game-info").each do 
-      time = datentime.css("span.g5-component--mlb-scores__date").text
-      
-      date_time << time
-      end
-      
-    puts date_time
-    
-    
   
-    
-    
-      
-    
-
 # end
+
+
+
 
 
