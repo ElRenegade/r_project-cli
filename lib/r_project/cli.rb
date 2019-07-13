9f8ec32e536cb283a3f547fcb1a26a1df5838061
Santiago Sanchez <chagostwo@yahoo.com>
2019-07-13 20:52:35 +0000

    added if statement for user inputs

--- lib/r_project/cli.rb
+++ lib/r_project/cli.rb
@@ -6,14 +6,25 @@ class RuneScapeUpdates::CLI
   end
   
   def start
-    puts "what update would you like to read about? Please select a number\n"
+    puts "Which update would you like to read about? Please select a number\n"
     print_titles
     input = gets.strip
     
     selection = RuneScapeUpdates::Updates.find(input.to_i)
    
     print_article(selection)
+    puts "Are you interested in another update summary? Y to read another or N to exit \n"
     
+    input = gets.strip.downcase
+    if input == "y"
+      start
+    elsif input == "n"
+      puts "Bye! Check in again for the latest OSRS update summaries!"
+      exit
+    else
+      puts "Invalid entry please try again"
+      start
+    end
     
   end
   
@@ -27,7 +38,7 @@ class RuneScapeUpdates::CLI
     puts "_____ #{selection.title}_____"
     puts "\n_____________Description____________"
     puts ""
-    puts "#{selection.description}"
+    puts "#{selection.description}\n\n"
   end
   
 end
